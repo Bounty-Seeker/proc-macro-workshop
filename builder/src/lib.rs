3473b488eb7976affba6cedfd0a516329daf7e4c
J. Spaul <12886021+Bounty-Seeker@users.noreply.github.com>
2021-05-24 02:09:48 +0100

    Completed part 2 of builder macro.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,39 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+use quote::{quote, format_ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(input as DeriveInput);
 
-    TokenStream::new()
+    // TokenStream::new()
+
+    // get ident of given type
+    let identi = input.ident;
+
+    // create ident of our builder
+    let builder_ident = format_ident!("{}Builder", identi);
+
+    // create the builder struct and functions
+    let output = quote!(
+        pub struct #builder_ident {
+        executable: Option<String>,
+        args: Option<Vec<String>>,
+        env: Option<Vec<String>>,
+        current_dir: Option<String>,
+        }
+
+        impl #identi {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        });
+
+    TokenStream::from(output)
 }
