5a25189651f7b9d2d3cafb8c1e165575eb925016
J. Spaul <12886021+Bounty-Seeker@users.noreply.github.com>
2021-05-29 21:05:54 +0100

    Completed first part of debug macro.

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,14 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TS2;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
 
-    unimplemented!()
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(input as DeriveInput);
+
+
+    let output = TS2::new();
+    output.into()
 }
