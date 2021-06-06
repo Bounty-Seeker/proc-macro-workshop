f265aa1155ffef9be632e1b55862631dea0be1bc
J. Spaul <12886021+Bounty-Seeker@users.noreply.github.com>
2021-06-06 04:43:57 +0100

    Completed part 2 of sorted macro. Needed to change output error as note was inaccurate.

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,26 +1,87 @@
 use proc_macro::TokenStream;
-use proc_macro2::TokenStream as TS2;
+use proc_macro2::{TokenStream as TS2, Span};
 use quote::ToTokens;
-use syn::{Item, parse_macro_input};
+//use quote::ToTokens;
+use syn::{Item, parse_macro_input, ItemEnum, Error, Result};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    //let _ = args;
 
     // Check no args
     if !args.is_empty() {
-        panic!()
+        let err = Error::new(Span::call_site(), "Unexpected args")
+                                    .into_compile_error()
+                                    .into();
+        return err;
     }
 
     // Parse input as syn::item
     let input_item : Item = parse_macro_input!(input as Item);
 
+    // enum_sorted
+    let output = sorted_macro(&input_item);
 
-    //println!("{:?}", args);
-    //println!("{:?}", input_item);
-
-
-    let mut output = TS2::new();
-    output.extend(input_item.into_token_stream());
-    output.into()
+    // Create output
+    match output {
+        Ok(()) => input_item.into_token_stream().into(),
+        Err(err) => { err.into_compile_error().into() }
+    }
 }
+
+/// Checks if Item is enum
+fn is_enum(item:&Item) -> Result<&ItemEnum> {
+
+    if let Item::Enum(item_enum) = item {
+        Ok(item_enum)
+    } else {
+        let err = Error::new(Span::mixed_site(), "expected enum or match expression");
+        Err(err)
+    }
+}
+
+/// Checks if enum is sorted
+fn _enum_is_sorted(_enu : &ItemEnum) -> Result<()> {
+    unimplemented!()
+}
+
+fn _match_sorted(_input_item : &Item) -> Result<TS2> {
+
+    // Parse input as syn::item
+    //let input_item : Item = parse_macro_input!(input as Item);
+
+//.map_err(Error::into_compile_error);
+
+    unimplemented!()
+}
+
+fn sorted_macro(input_item: &Item) -> Result<()> {
+
+    // Check if enum
+    let _input_enum = is_enum(input_item)?;
+
+    //match Ok
+
+    Ok(())
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
