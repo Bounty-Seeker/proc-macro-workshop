f96c976a7ad333caca7eac3b22db6e19b2b6cd9e
J. Spaul <12886021+Bounty-Seeker@users.noreply.github.com>
2021-06-06 02:27:53 +0100

    Completed part one of sorted macro.

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,26 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TS2;
+use quote::ToTokens;
+use syn::{Item, parse_macro_input};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
+    //let _ = args;
 
-    unimplemented!()
+    // Check no args
+    if !args.is_empty() {
+        panic!()
+    }
+
+    // Parse input as syn::item
+    let input_item : Item = parse_macro_input!(input as Item);
+
+
+    //println!("{:?}", args);
+    //println!("{:?}", input_item);
+
+
+    let mut output = TS2::new();
+    output.extend(input_item.into_token_stream());
+    output.into()
 }
