09ffcf69d0c39b5c4878e59adfcb62d496079277
J. Spaul <12886021+Bounty-Seeker@users.noreply.github.com>
2021-06-06 05:34:28 +0100

    Completed part three of sorted macro.

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -9,9 +9,9 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
 
     // Check no args
     if !args.is_empty() {
-        let err = Error::new(Span::call_site(), "Unexpected args")
-                                    .into_compile_error()
-                                    .into();
+        let err = Error::new_spanned(TS2::from(args), "Unexpected args")
+                                .into_compile_error()
+                                .into();
         return err;
     }
 
@@ -34,14 +34,52 @@ fn is_enum(item:&Item) -> Result<&ItemEnum> {
     if let Item::Enum(item_enum) = item {
         Ok(item_enum)
     } else {
-        let err = Error::new(Span::mixed_site(), "expected enum or match expression");
+        let err = Error::new(Span::call_site(), "expected enum or match expression");
         Err(err)
     }
 }
 
 /// Checks if enum is sorted
-fn _enum_is_sorted(_enu : &ItemEnum) -> Result<()> {
-    unimplemented!()
+fn enum_is_sorted(enu : &ItemEnum) -> Result<()> {
+
+    // get intial variant
+    let prev_ident = match enu.variants.first() {
+        Some(prev_vari) => &prev_vari.ident,
+        None => return Ok(())
+    };
+
+    // check each variant
+    for variant in &enu.variants {
+
+        // get variant ident
+        let vari_ident = &variant.ident;
+
+        // if previous ident is less than current ident continue
+        if prev_ident <= vari_ident {
+            continue;
+        }
+
+        // now create error as not sorted
+
+        // get ident the misplaced ident should be before
+        let mut variants = enu.variants.iter()
+        .map(|variant| {
+            &variant.ident
+        }).skip_while(|cur_ident|  *cur_ident <= vari_ident );
+
+        // Unwrap doesn't panic as we know element with greater ident exists
+        let sort_before_ident = variants.next().unwrap();
+        //println!("first: {:?}", first);
+
+        // construct error msg
+        let err_msg = format!("{} should sort before {}", vari_ident, sort_before_ident);
+
+        // finally construct and return error
+        let err = Error::new(vari_ident.span(), err_msg);
+        return Err(err);
+    }
+
+    Ok(())
 }
 
 fn _match_sorted(_input_item : &Item) -> Result<TS2> {
@@ -57,11 +95,10 @@ fn _match_sorted(_input_item : &Item) -> Result<TS2> {
 fn sorted_macro(input_item: &Item) -> Result<()> {
 
     // Check if enum
-    let _input_enum = is_enum(input_item)?;
+    let input_enum = is_enum(input_item)?;
 
-    //match Ok
-
-    Ok(())
+    // Check if sorted
+    enum_is_sorted(input_enum)
 }
 
 
