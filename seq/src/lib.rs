f732f3ab7aa0af2065aba53f991d61c896866a45
J. Spaul <12886021+Bounty-Seeker@users.noreply.github.com>
2021-06-10 10:00:23 +0100

    Part one of seq macro completed.

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,42 @@
 use proc_macro::TokenStream;
+use proc_macro2::{TokenStream as TS2};
+use syn::parse_macro_input;
+use seq_parsing::SeqParsed;
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let _parsed_seq = parse_macro_input!(input as SeqParsed);
 
-    unimplemented!()
+
+
+    let output = TS2::new();
+    output.into()
+}
+
+
+mod seq_parsing {
+use syn::{Ident, Token, braced, parse::Parse, token::Brace};
+
+    pub struct SeqParsed {
+        initial_ident:Ident,
+        in_token: Token![in],
+        start_val : syn::LitInt,
+        dot2 : Token![..],
+        end_val : syn::LitInt,
+        braced : Brace,
+    }
+
+    impl Parse for SeqParsed {
+        fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+            let _content;
+        Ok(SeqParsed{
+            initial_ident: input.parse()?,
+            in_token: input.parse()?,
+            start_val:input.parse()?,
+            dot2: input.parse()?,
+            end_val:input.parse()?,
+            braced: braced!(_content in input),
+        })
+    }
+    }
 }
